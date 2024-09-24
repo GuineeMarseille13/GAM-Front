857438955833ce9502b5bad759f14bde91bbd6a0
Aboubacar Sidig DIALLO <asd1194diallo@gmail.com>
2024-09-24 15:25:34 +0200

    amélioration section evenement

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCircleCheck, faFile } from '@fortawesome/free-regular-svg-icons';
+import { faCircleCheck, faCircleRight } from '@fortawesome/free-regular-svg-icons';
 import { PoleItems } from 'src/app/types/enums/poleItems.enum';
 import { GalleriaModule } from 'primeng/galleria';
 import { PhotoService } from 'src/app/shared/photo.service';
@@ -54,7 +54,7 @@ export class MainComponent {
   protected canShowEven: boolean = false;
 
   protected faCircleCheck = faCircleCheck;
-  protected faFile = faFile;
+  protected faCircleRight = faCircleRight;
 
   responsiveOptions: any[] | undefined;
   products: Product[] = [];
