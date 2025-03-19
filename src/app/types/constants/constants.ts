a36958f663acc3052b28d79752fc3620ab1ad885
= <ascode.alpha@gmail.com>
2025-03-19 22:05:04 +0100

    feat: mise à jour de la consultation des photos des differents pôles

--- src/app/types/constants/constants.ts
+++ src/app/types/constants/constants.ts
@@ -12,3 +12,24 @@ export const responsivePhotosOptions: ResponsivePhotoOption[] = [
       numVisible: 1,
     },
   ];
+
+
+  export const IMAGE_ITEMS_ADMIN: any[] = [
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 1.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 2.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 3.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 4.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 5.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 6.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 7.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 8.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 9.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 10.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 11.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 12.jpg', alt: 'Image 1' },
+    { itemImageSrc: 'assets/pole/accompagnement/Photos 3.jpg', alt: 'Image 1' },
+];
+
+  export const IMAGE_ITEMS_EVEN: any[] = [];
+
+  export const IMAGE_ITEMS_HEBERGEMENT: any[] = [];
