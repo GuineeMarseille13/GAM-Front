0ec47725c5e4fc92b45e7768aa830e3a13b4cee7
Aboubacar Sidig DIALLO <asd1194diallo@gmail.com>
2025-04-22 21:11:16 +0200

    Creation du composant

---
+++ src/app/pages/poles/administratif/administratif.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'administratif',
+  standalone: true,
+  imports: [],
+  templateUrl: './administratif.component.html',
+  styleUrl: './administratif.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AdministratifComponent {}
