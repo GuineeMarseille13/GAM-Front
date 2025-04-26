1faa4826bbc5ac4427c1fef08cf647fc743366e6
Aboubacar Sidig DIALLO <asd1194diallo@gmail.com>
2025-04-26 23:33:02 +0200

    Optimisation et amélioration

--- src/app/composants/tabView/tabView.component.ts
+++ src/app/composants/tabView/tabView.component.ts
@@ -3,23 +3,30 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFile } from '@fortawesome/free-regular-svg-icons';
 import { TabItems } from 'src/app/types/enums/TabItems.enum';
-import { ProfilMembreBureauComponent } from "../profil-membre-bureau/profil-membre-bureau.component";
+import { ProfilMembreBureauComponent } from '../profil-membre-bureau/profil-membre-bureau.component';
 import { RapportActiviteService } from 'src/app/services/rapport-activite.service';
 import { InfoRapportActivite } from 'src/app/types/interfaces/info-rapport-activite';
-import { RapportActiviteComponent } from "../rapport-activite/rapport-activite.component";
+import { RapportActiviteComponent } from '../rapport-activite/rapport-activite.component';
 import { MembreBureauInfo } from 'src/app/types/interfaces/membre-bureau-info';
 import { MembreBureauInfoService } from 'src/app/services/membre-bureau-info.service';
-import { MotDuPresidentComponent } from "./mot-du-president/mot-du-president.component";
+import { MotDuPresidentComponent } from './mot-du-president/mot-du-president.component';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'tabView',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule, ProfilMembreBureauComponent, RapportActiviteComponent, MotDuPresidentComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    ProfilMembreBureauComponent,
+    RapportActiviteComponent,
+    MotDuPresidentComponent,
+  ],
   templateUrl: './tabView.component.html',
   styleUrl: './tabView.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabViewComponent implements OnInit{
+export class TabViewComponent implements OnInit {
   protected tabItems = TabItems;
   protected rapportsActivites: InfoRapportActivite[] = [];
   protected membreBureauInfos: MembreBureauInfo[] = [];
@@ -31,11 +38,32 @@ export class TabViewComponent implements OnInit{
 
   protected faFile = faFile;
 
-  constructor(private rapportActiviteService: RapportActiviteService, private membreBureauInfoService: MembreBureauInfoService) {}
+  constructor(
+    private rapportActiviteService: RapportActiviteService,
+    private membreBureauInfoService: MembreBureauInfoService,
+    private router: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.rapportActiviteService.getRapportsActivites().subscribe(ra => this.rapportsActivites = ra);
-    this.membreBureauInfoService.getMembreBureauInfos().subscribe(mbi => this.membreBureauInfos = mbi);
+    // Là on écoute le paramètre tabItem de l'URL
+    this.router.params.subscribe((params) => {
+      const tabItemParam = params['tabItem'] as TabItems;
+
+      // Si un paramètre tabItem est présent, on active l'onglet correspondant
+      if (tabItemParam && Object.values(this.tabItems).includes(tabItemParam)) {
+        this.getActiveTabItem(tabItemParam);
+      } else {
+        // Sinon, on active l'onglet par défaut (Président)
+        this.getActiveTabItem(this.tabItems.President);
+      }
+    });
+
+    this.rapportActiviteService
+      .getRapportsActivites()
+      .subscribe((ra) => (this.rapportsActivites = ra));
+    this.membreBureauInfoService
+      .getMembreBureauInfos()
+      .subscribe((mbi) => (this.membreBureauInfos = mbi));
   }
 
   private desableAllTabItems(): void {
@@ -45,7 +73,7 @@ export class TabViewComponent implements OnInit{
     this.isNotreEquipeActive = false;
   }
 
-  protected activeTabItem(tabItem: TabItems): void {
+  protected getActiveTabItem(tabItem: TabItems): void {
     this.desableAllTabItems();
     switch (tabItem) {
       case this.tabItems.President:
