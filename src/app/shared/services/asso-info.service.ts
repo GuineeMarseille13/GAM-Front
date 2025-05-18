c70ff140aa3fb675137c60e0674d7bfc7ee9fefb
Aboubacar Sidig DIALLO <asd1194diallo@gmail.com>
2025-05-18 18:38:48 +0200

    Update: refacto globale

--- src/app/shared/services/asso-info.service.ts
+++ src/app/shared/services/asso-info.service.ts
@@ -2,31 +2,30 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AssoInfoService {
-
-  constructor() { }
+  constructor() {}
 
   private getInfos(): Observable<any> {
     return of({
-      name: "Guinée à Marseille",
-      dateCreation: "17/05",
-      anneCreation: "2021",
-      tel: "+33767133928",
-      mail: "guineeamarseille13@gmail.com",
+      name: 'Guinée à Marseille',
+      dateCreation: '17/05',
+      anneCreation: '2021',
+      tel: '+33767133928',
+      mail: 'guineeamarseille13@gmail.com',
       reseauxSociaux: {
-        facebook: "https://www.facebook.com/GUINEEAMARSEILLE",
-        tiktok: "",
-        instagram: "https://www.instagram.com/guineeamarseille/",
-        x: "http://twitter.com/GuineeMarseill1",
-        linkedIn: ""
-      }
-    })
+        facebook: 'https://www.facebook.com/GUINEEAMARSEILLE',
+        tiktok: 'https://www.tiktok.com/@guineeamarseille',
+        instagram: 'https://www.instagram.com/guineeamarseille/',
+        x: 'http://twitter.com/GuineeMarseill1',
+        linkedIn:
+          'https://fr.linkedin.com/posts/guin%C3%A9e-%C3%A0-marseille_solidarit%C3%A9-association-migrations-activity-7300156349592621058-UHjb',
+      },
+    });
   }
 
   public getAssoInfos(): Observable<any> {
     return this.getInfos();
   }
-
 }
